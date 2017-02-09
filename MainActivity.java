d02d2fdc10e5b114dc251c45faa17778f0711f5e
evgenychapaev <lkji5010@gmail.com>
2017-02-09 23:21:22 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.zver.textedit;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends ActionBarActivity {
+    private final static String FILENAME = "sample.txt"; // имя файла
+    private EditText mEditText;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_open:
+                openFile(FILENAME);
+                return true;
+            case R.id.action_save:
+                saveFile(FILENAME);
+                return true;
+            default:
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent();
+                intent.setClass(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+    }
+
+    // Метод для открытия файла
+    private void openFile(String fileName) {
+        try {
+            InputStream inputStream = openFileInput(fileName);
+
+            if (inputStream != null) {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                inputStream.close();
+                mEditText.setText(builder.toString());
+            }
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Метод для сохранения файла
+    private void saveFile(String fileName) {
+        try {
+            OutputStream outputStream = openFileOutput(fileName, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+            osw.write(mEditText.getText().toString());
+            osw.close();
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        // читаем установленное значение из CheckBoxPreference
+        if (prefs.getBoolean(getString(R.string.pref_openmode), false)) {
+            openFile(FILENAME);
+
+// читаем размер шрифта из EditTextPreference
+            float fSize = Float.parseFloat(prefs.getString(
+                    getString(R.string.pref_size), "20"));
+// применяем настройки в текстовом поле
+            mEditText.setTextSize(fSize);
+            // читаем стили текста из ListPreference
+            String regular = prefs.getString(getString(R.string.pref_style), "");
+            int typeface = Typeface.NORMAL;
+
+            if (regular.contains("Полужирный"))
+                typeface += Typeface.BOLD;
+
+            if (regular.contains("Курсив"))
+                typeface += Typeface.ITALIC;
+
+// меняем настройки в EditText
+            mEditText.setTypeface(null, typeface);
+        }
+    }
+}
